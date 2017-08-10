4ebbab4696bbd0ba2fa38dd54405955c42eb9256
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-08-09 23:32:22 -0300

    Configuring 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+<<<<<<< HEAD
   before_action :authenticate_user!
+=======
+  #before_action :authenticate_user!
+>>>>>>> Configuring 404
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def render_404
