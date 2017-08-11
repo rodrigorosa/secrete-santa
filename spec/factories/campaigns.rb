c74eb03bea2008539cefbc64d129d0454952511b
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-08-11 18:23:48 -0300

    Fixing campaing methods

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
-  factory :campaign do
-    title { FFaker::Lorem.word }
-    description { FFaker::Lorem.sentence }
-    user
-  end
+ factory :campaign do
+   title         { FFaker::Lorem.word }
+   description   { FFaker::Lorem.sentence }
+   user
+   status        { :pending }
+   location      { "#{FFaker::Address.city}, #{FFaker::Address.street_address}"}
+   event_date    { FFaker::Time.date }
+   event_hour    { rand(24).to_s }
+ end
 end
