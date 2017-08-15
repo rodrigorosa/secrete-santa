f6c2e956988440c5a2378b6a022b726fc853daa5
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-08-14 21:19:25 -0300

    Configuring nginx

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-  before_action :authenticate_user!
-=======
-  #before_action :authenticate_user!
->>>>>>> Configuring 404
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def render_404
